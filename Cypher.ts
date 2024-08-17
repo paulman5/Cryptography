845eb4a9fdcfb2a7fb23005622cef7380ed87d21
Paul <mierlo.paul@hotmail.nl>
2024-08-17 16:01:38 +0200

    emperor cypher version2

--- Cypher.ts
+++ Cypher.ts
@@ -1,13 +1,21 @@
 let emperor_message = "NHAG JBEEL CHMMYR XVJV"
 let emperor_messagesplit = emperor_message.split("")
 
+console.log(emperor_messagesplit)
+
 for (let cipher_counter = 1; cipher_counter <= 26; cipher_counter++) {
   let decryptedmessage = ""
   for (let i = 0; i < emperor_messagesplit.length; i++) {
+    let lowerbound = 65
     let asciiValue = emperor_messagesplit[i].charCodeAt(0)
-    let newAsciiValue = asciiValue - cipher_counter
-    console.log(newAsciiValue)
-    decryptedmessage += String.fromCharCode(newAsciiValue)
+    if (asciiValue - cipher_counter <= lowerbound) {
+      let newAsciiValue = asciiValue - cipher_counter + 26
+      console.log(newAsciiValue)
+      decryptedmessage += String.fromCharCode(newAsciiValue)
+    } else {
+      let newAsciiValue = asciiValue - cipher_counter
+      decryptedmessage += String.fromCharCode(newAsciiValue)
+    }
   }
   console.log(decryptedmessage)
 }
