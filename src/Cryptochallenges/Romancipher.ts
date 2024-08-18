280126c01ca26b485e06e644697884c90fa0df66
Paul <mierlo.paul@hotmail.nl>
2024-08-18 23:53:46 +0200

    completed romancipher

--- src/Cryptochallenges/Romancipher.ts
+++ src/Cryptochallenges/Romancipher.ts
@@ -7,10 +7,14 @@ console.log(emperor_messagesplit)
 for (let shift = 1; shift < 26; shift++) {
   let decryptedmessage = ""
   for (let i = 0; i < emperor_messagesplit.length; i++) {
-    let newAsciivalue = String.fromCharCode(
-      ((emperor_messagesplit[i].charCodeAt(0) + shift - 65) % 26) + 65
-    )
-    decryptedmessage += newAsciivalue
+    if (emperor_messagesplit[i] === " ") {
+      decryptedmessage += " "
+    } else {
+      let newAsciivalue = String.fromCharCode(
+        ((emperor_messagesplit[i].charCodeAt(0) + shift - 65) % 26) + 65
+      )
+      decryptedmessage += newAsciivalue
+    }
   }
   console.log(decryptedmessage)
 }
@@ -19,14 +23,18 @@ for (let shift = 1; shift < 26; shift++) {
 for (let shift = 1; shift < 26; shift++) {
   let decryptedmessage = ""
   for (let i = 0; i < emperor_messagesplit.length; i++) {
-    let asciiValue = emperor_messagesplit[i].charCodeAt(0)
-    let newAsciiValue
-    if (asciiValue - shift < 65) {
-      newAsciiValue = 90 - (65 - (asciiValue - shift)) + 1
+    if (emperor_messagesplit[i] === " ") {
+      decryptedmessage += " "
     } else {
-      newAsciiValue = asciiValue - shift
+      let asciiValue = emperor_messagesplit[i].charCodeAt(0)
+      let newAsciiValue
+      if (asciiValue - shift < 65) {
+        newAsciiValue = 90 - (65 - (asciiValue - shift)) + 1
+      } else {
+        newAsciiValue = asciiValue - shift
+      }
+      decryptedmessage += String.fromCharCode(newAsciiValue)
     }
-    decryptedmessage += String.fromCharCode(newAsciiValue)
   }
   console.log(decryptedmessage)
 }
